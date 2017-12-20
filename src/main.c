6f2ad0caab9655e2bbdc3220c969be9789c36439
asuun <alvarsuun@gmail.com>
2017-12-20 21:24:51 +0000

    veakonsooli ja assert() testimine

--- src/main.c
+++ src/main.c
@@ -8,19 +8,26 @@
 
 void main ()
 {
-    /* Set port B pin 7 for output for Arduino Mega yellow LED */
+    /* Set port B pin 7 for output for Arduino Mega yellow LED 
     DDRB |= _BV(DDB7);
-    _delay_ms(BLINK_DELAY_MS3);
-    PORTB &= ~_BV(PORTB7);
-    /*
+    */
     DDRA |= _BV(DDA0);      // Pin 22 seatakse outputiks Punane LED 
     DDRA |= _BV(DDA2);      // Pin 24 seatakse outputiks Valge LED
     DDRA |= _BV(DDA4);      // Pin 26 seatakse outputiks Roheline LED
+    
     while (1) {
+            /*
+             PORTB |= _BV(PORT7);           // Punane LED polema
+            _delay_ms(BLINK_DELAY_MS3);
+            PORTB &= ~_BV(PORTB7);  
+              _delay_ms(BLINK_DELAY_MS3);
+               */
+               
+            PORTB &= ~_BV(PORTB7);
             PORTA |= _BV(PORTA0);           // Punane LED polema
             PORTA &= ~_BV(PORTA2);      // Valge LED kustu
             PORTA &= ~_BV(PORTA4);      // Roheline LED kustu
-
+    
             _delay_ms(BLINK_DELAY_MS1);
             PORTA &= ~_BV(PORTA0);      // Punane LED kustu
             PORTA |= _BV(PORTA2);       // Valge LED polema
@@ -32,5 +39,5 @@ void main ()
             _delay_ms(BLINK_DELAY_MS3);
 
         }
-        */
+       
     }
